ca8f42956856975262e319d685480a246de1119f
Gabriel Ratener <gabe9693@gmail.com>
2019-10-06 15:42:08 -0700

    Add support for stateful lexing, and improve pattern API

--- index.mjs
+++ index.mjs
@@ -1,21 +1,40 @@
 
 /****************************
- * A lexer in under 200 lines of code
+ * A lexer in under 350 lines of code
  * Dunno why, but felt like sharing
  ****************************/
 export default class Lexer {
-    constructor(patterns) {
-        this.patterns = patterns.map(o => Object.freeze(o));
-        this.tokenPrototype = {
-            get string() {
-                return this.source.slice(...this.range);
-            }
-        };
-        this._regex = this._getRegex(patterns);
-        this._cache = new Map;
+    constructor(settings) {
+        const states =
+          Array.isArray(settings) ?
+            {start: settings} :
+            settings.states;
+
+        this.states =
+          Object.entries(states)
+            .reduce((map, [name, patterns]) => {
+                map.set(name, patterns);
+
+                return map;
+            }, new Map());
+
+        this._regexes = new Map(/* state => pattern list */);
+        this._cache = new Map();
+
+        for (const state of this._getStates()) {
+            const patterns = this.states.get(state);
+
+            this._regexes.set(state, this._getRegex(patterns));
+        }
     }
     
-    _getRegex(patterns = this.patterns) {
+    _getStateRegex(state = 'start') {
+        const patterns = this.states.get(state);
+
+        return this._getRegex(patterns);
+    }
+
+    _getRegex(patterns) {
         let i = 0, concatenation = "";
         for (let {regex} of patterns) {
             if (i > 0)
@@ -30,90 +49,213 @@ export default class Lexer {
         return `(?:${concatenation})`;
     }
     
-    _getRemainingRegex(index, cache = this._cache) {
-        if (cache.has(index)) {
-            return cache.get(index);
+    _getRemainingRegex(index, state = 'start', cache = this._cache) {
+        const key = `${state}-${index}`;
+
+        if (cache.has(key)) {
+            return cache.get(key);
         } else {
-            const source = this._getRegex(this.patterns.slice(index + 1));
+            const patterns = this.states.get(state);
+            const source = this._getRegex(patterns.slice(index + 1));
             const regex = new RegExp(source, 'y');
                         
-            cache.set(index, regex);
+            cache.set(key, regex);
             
             return regex;
         }
     }
     
-    _match(regex, input, getContext, oldgex = regex) {
-        
+    _match(regex, input, getContext, state = 'start', oldgex = regex) {
         let index = regex.lastIndex;
         let match = regex.exec(input);
         
         if (match === null)
-            return this.patterns.length + 1;
+            return -1;
 
         for (let i = 1; i < match.length; i++) {
             if (match[i] !== undefined) {
                 
-                const pattern = this.patterns[i - 1];
-                const {test} = pattern;
+                const pattern = this._getPattern(state, i - 1);
                 
-                if (test && !test.apply(pattern, [getContext(match[0], index)])) {
-                    const newgex = this._getRemainingRegex(i - 1);
+                if (pattern.test && !pattern.test(getContext(match[i], index))) {
+                    const newgex = this._getRemainingRegex(i - 1, state);
                     newgex.lastIndex = index;
-                    return i + this._match(newgex, input, getContext, oldgex);
+                    return i + this._match(newgex, input, getContext, state, oldgex);
                 } else {
                     oldgex.lastIndex = regex.lastIndex;
                     return i - 1;
                 }
             }
         }
+
+        return -1;
     }
     
+    * _getStates() {
+        for (const key in this.states) {
+            if (this.states.hasOwnProperty(key)) {
+                yield key;
+            }
+        }
+    }
+
+    token({type, value}, source = "", range = [0, 0]) {
+        // overwrite this to change default behavior
+
+        return {
+            type,
+            value,
+            range,
+            string: source.slice(...range),
+
+            get source() {
+                return source;
+            }
+        }
+    }
+
+    _getPattern(state, index) {
+        const patterns = this.states.get(state);
+
+        return patterns[index];
+    }
+
     // tokenize a complete input string
     * tokenize(str = '', start = 0) {
-        const regex = new RegExp(this._regex, 'y');
-        const token = (type, range, value = str.slice(...range)) => ({
-            type,
-            range,
-            value,
-            source: str,
-        });
+        const regexes = new Map();
+
+        const token = (type, range, value = str.slice(...range)) => {
+            const base = {type, value};
+
+            return this.token(base, str, range);
+        }
         
         const testContext = (match, start = 0) => Object.freeze({
             match,
             start,
             input: str,
             lexer: this,
+            
             lastToken() {
                 return lastToken;
+            },
+
+            get state() {
+                return getState();
             }
         });
+
+        const setStateRegex = (state = getState()) => {
+            if (!regexes.has(state)) {
+                const source = this._getStateRegex(state);
+                const regex = new RegExp(source, 'y');
+
+                regexes.set(state, regex);
+            }
+
+            if (regex) {
+                const {lastIndex} = regex;
                 
-        let i = regex.startIndex = regex.lastIndex = start;
+                regex = regexes.get(state);
+
+                regex.lastIndex = lastIndex;
+            } else {
+                regex = regexes.get(state);
+                regex.lastIndex = start;
+            }            
+        }
+
+        const getState = () =>
+          states.length > 0 ?
+            states[states.length - 1] :
+            'start';
+        
+        const stateControlAPI = {
+            pushState(state) {
+                if (state === 'start') {
+                    throw new Error('Cannot push state "start"!');
+                } else {
+                    states.push(state);
+
+                    setStateRegex(getState());
+                }
+            },
+            popState() {
+                if (states.length > 0) {
+                    states.pop();
+
+                    setStateRegex(getState());
+                } else {
+                    throw new Error('Cannot pop state');
+                }
+            }
+        }
+
+        // the state stack
+        const states = [];
+
         let lastToken = null;
+        let i = start;
+        let regex = null;
+
+        setStateRegex('start');
+
         while (i < str.length) {
             const start = regex.lastIndex;
 
-            const index = this._match(regex, str, testContext);
-            if (index === null || index > this.patterns.length)
+            const index = this._match(regex, str, testContext, getState());
+            if (index < 0) {
                 throw new Error("No valid matches found!");
-            else {
+
+            } else {
                 const match = str.slice(start, regex.lastIndex);            
-                const pattern = this.patterns[index];
-                const {fetch, type} = pattern;
+                const pattern = this._getPattern(getState(), index);
+
+                const sharedAPI = {
+                    input: str,
+                    lexer: this,
+
+                    get match() {
+                        return match;
+                    },
+                    get state() {
+                        return getState();
+                    },
+                    get position() {
+                        return i;
+                    }
+                }
+
                 let value;
+                let abort = false;
+
+                if (pattern.before) {
+                    const api = {
+                        ...sharedAPI,
+
+                        ...stateControlAPI,
+
+                        abort() {
+                            abort = true;
+                        }
+                    }
+
+                    pattern.before(api);
+                }
+
+                if (abort) {
+                    i = start;
+                    regex.lastIndex = start;
+
+                    continue;
+                }
 
                 i = regex.lastIndex;
 
-                if (fetch) {
+                if (pattern.fetch) {
                     const getVal = () => str.slice(start, i);
                     const context = Object.freeze({
-                        input: str,
-                        lexer: this,
-
-                        get position() {
-                            return i;
-                        },
+                        ...sharedAPI,
 
                         advanceTo(index) {
                             if (index < i) {
@@ -144,20 +286,28 @@ export default class Lexer {
                         }
                     });
 
-                    value = fetch.apply(pattern, [context, getVal]);
+                    pattern.fetch(context, getVal);
+                }
+
+                if (pattern.value) {
+                    const api = {
+                        ...sharedAPI
+                    }
+
+                    value = pattern.value(api);
                 }
 
                 {
                     let resolvedType;
-                    switch(typeof type) {
+                    switch(typeof pattern.type) {
                         case "undefined":
                             resolvedType = match;
                             break;
                         case "string":
-                            resolvedType = type;
+                            resolvedType = pattern.type;
                             break;
                         case "function":
-                            resolvedType = type(match);
+                            resolvedType = pattern.type(match);
                             break;
                     }
 
@@ -166,6 +316,18 @@ export default class Lexer {
                         token(resolvedType, [start, i], value);
                     yield lastToken;
 
+                    if (pattern.after) {
+                        const api = {
+                            ...sharedAPI,
+                            ...stateControlAPI,
+
+                            get token() {
+                                return lastToken;
+                            }
+                        }
+
+                        pattern.after(api);
+                    }   
                 }
 
                 regex.lastIndex = i;
--- test.mjs
+++ test.mjs
@@ -1,31 +1,124 @@
 
-// Not a test suite or anything, just an informal test script
-
+import tape from "tape"
 import Lexer from "./index"
 
-const text = `
-	12 + 987 + baby %is %exyaession
-	a b c %yello 77 + 9 -
-`;
+tape('Basic Lexing', (t) => {
 
-const lexer = new Lexer([
-	{regex: /[\*\/\+\-\^]/},
-	{regex: /[ \t\n]+/, type: 'ws'},
-	{regex: /[a-z]+/, type: 'id'},
-	{regex: /[0-9]+/, type: 'int'},
+	t.plan(4);
+
 	{
-		regex: /\%[a-z]+/,
-		type: (val) => val.slice(1),
-		test(ctxt) {
-			return ctxt.match.includes('ya');
-		}
-	},
-	{
-		regex: /\%[a-z]+/,
-		type: 'invalid'
+		const string = `12 + baby %is %exyaession`;
+		const lexer = new Lexer([
+			{regex: /[\*\/\+\-\^]/},
+			{regex: /[ \t\n]+/, type: 'ws'},
+			{regex: /[a-z]+/, type: 'id'},
+			{regex: /[0-9]+/, type: 'int'},
+			{
+				regex: /\%[a-z]+/,
+				type: (val) => val.slice(1),
+				test(ctxt) {
+					return ctxt.match.includes('ya');
+				}
+			},
+			{
+				regex: /\%[a-z]+/,
+				type: 'invalid'
+			}
+		]);
+
+		const tokens = Array.from(lexer.tokenize(string));
+
+		t.equal(tokens[0].type, 'int');
+		t.equal(tokens[0].string, '12');
+
+		t.equal(tokens[6].type, 'invalid');
+		t.equal(tokens[6].string, '%is');
 	}
-]);
+});
 
-for (let {type, value} of lexer.tokenize(text)) {
-	console.log(type, value);
-}
+tape('Tokens with custom values', (t) => {
+
+	t.plan(4);
+
+	{
+		const string = `baby + 12 food`;
+		const lexer = new Lexer([
+			{regex: /[\*\/\+\-\^]/},
+			{regex: /[ \t\n]+/, type: 'ws'},
+			{regex: /[a-z]+/, type: 'id'},
+			{
+				regex: /[0-9]+/,
+				type: 'int',
+				value({match}) {
+					return parseInt(match, 10);
+				}
+			}
+		]);
+
+		const tokens = Array.from(lexer.tokenize(string));
+
+		t.equal(tokens[4].type, 'int');
+		t.equal(tokens[4].string, '12');
+		t.equal(typeof tokens[4].value, 'number');
+		t.equal(tokens[4].value, 12);
+	}
+});
+
+tape('Multi-state Lexing', (t) => {
+	t.plan(10);
+
+	{
+		// lex scaled down html/jsx syntax
+		const lexer = new Lexer({
+			states: {
+				'start': [
+					{
+						regex: /[^\<\>\{\}]+/,
+						type: 'text'
+					},
+					{
+						regex: /\{[a-z]+\}/,
+						type: 'expression'
+					},
+					{
+						regex: /\</,
+						before(api) {
+							api.pushState('tag');
+							api.abort();
+						}
+					}
+				],
+				"tag": [
+					{
+						regex: /[\<\>\/]/,
+						after(api) {
+							if (api.token.type === '>') {
+								api.popState();
+							}
+						}
+					},
+					{
+						regex: /[a-zA-Z]+/,
+						type: 'id'
+					}
+				]
+			}
+		});
+	
+		const string = '<hello>this is some {random} text</hello>';
+		const tokens = Array.from(lexer.tokenize(string));
+
+		t.equals(tokens[0].type, '<');
+		t.equals(tokens[0].value, '<');
+		t.equals(tokens[3].type, 'text');
+		t.equals(tokens[3].value, 'this is some ');
+
+		t.equals(tokens[4].type, 'expression');
+		t.equals(tokens[4].value, '{random}');
+		t.equals(tokens[5].type, 'text');
+		t.equals(tokens[5].value, ' text');
+
+		t.equals(tokens[7].type, '/');
+		t.equals(tokens[7].value, '/');
+	}
+});
